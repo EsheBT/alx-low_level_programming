fd0dbdc18fd41fd2f8fe39c1f600843c3309397f
EsheBT <eshetu9244@yahoo.com>
2022-12-08 12:10:45 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+ * betty now fixed and corrected to write comment for my program in the future
+ */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
