097bf9f1227c9f0133512017ef692cf89d14f34d
EsheBT <eshetu9244@yahoo.com>
2022-12-09 11:52:51 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,19 +8,21 @@
 * main - entry main
 *
 *Return:always return zero
+*/
 int main(void)
 {
 	int n;
 	int m;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("last digit of %d is ", n);
-	m=n % 19;
-	if m > 5)
+	m = n % 10;
+	if (m > 5)
 	{
 		printf("%d and is grater than 5\n", m);
 	}
-	else if ((m < 6) && (m <0))
+	else if ((m < 6) && (m < 0))
 	{
 		printf("%d and is less than 6 and not 0\n", m);
 	}
