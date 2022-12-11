cc6b49fccce55c03d1d35e7800540e3fbf7478ca
EsheBT <eshetu9244@yahoo.com>
2022-12-11 05:21:59 -0800

    numbers 0 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The primary function
+ *
+ *
+ * Return: end the function execusion
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = '0' ; m <= '9' ; m++)
+	{
+	printf(m);
+
+	}
+	printf('\n');
+
+	return (0);
+}
