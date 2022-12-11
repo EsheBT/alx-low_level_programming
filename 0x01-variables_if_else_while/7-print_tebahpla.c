34c1b5201be587ad9ff29f71aa661a01ae9a6f3f
EsheBT <eshetu9244@yahoo.com>
2022-12-11 06:06:12 -0800

    alphabet z a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The primary function
+ *
+ *
+ * Return: end the function execusion
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = 'z' ; m <= 'a' ; m--)
+	{
+	putchar(m);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
