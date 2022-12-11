ec8dc59e9016cb710e2c5ba04483f13b1bb581b0
EsheBT <eshetu9244@yahoo.com>
2022-12-11 08:26:37 -0800

    threev digit numbers combinations in ascendinding

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,23 @@
 
 int main(void)
 {
-	int x1, x2;
+	int x1, x2, x3;
 
-	for (x1 = 0; x1 < 9; x1++)
+	for (x1 = 0; x1 < 8; x1++)
 	{
-		for (x2 = x1 + 1; x2 < 10; x2++)
+		for (x2 = x1 + 1; x2 < 9; x2++)
+		{
+		for (x3 = x2 + 1; x3 < 10; x3++)
 		{
 			putchar((x1 % 10) + '0');
 			putchar((x2 % 10) + '0');
+			putchar((x3 % 10) + '0');
 
-			if (x1 == 8 && x2 == 9)
+			if (x1 == 7 && x2 == 8 && x3 == 9)
 			continue;
 			putchar(',');
 			putchar(' ');
+		}
 		}
 	}
 
@@ -29,3 +33,4 @@ int main(void)
 
 	return (0);
 }
+
