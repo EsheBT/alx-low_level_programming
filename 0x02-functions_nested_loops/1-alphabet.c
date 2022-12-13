4f5a8a3ac12f65b8fd612358b817d6796cf2feb4
EsheBT <eshetu9244@yahoo.com>
2022-12-13 10:18:24 -0800

    a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ * Discription: printing lower case alphabets
+ * Return: (0) success
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
+
