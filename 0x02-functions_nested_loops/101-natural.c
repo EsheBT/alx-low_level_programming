45ffb62599f186b32c312cc0d64ab1c5f69b8466
EsheBT <eshetu9244@yahoo.com>
2022-12-14 12:58:51 -0800

    prime sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - computes and prints the sum of all the multiples of
+  * 3 or 5 below 1024.
+  * Return: Nothing.
+  */
+
+int main(void)
+{
+	int p, sum;
+
+	for (p = 0; p < 1024; p++)
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += p;
+	printf("%d\n", sum);
+	return (0);
+}
