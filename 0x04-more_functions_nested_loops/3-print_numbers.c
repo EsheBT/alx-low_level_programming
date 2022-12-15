7b1446f92d7dcda9efec654610f2e60dacd08337
EsheBT <eshetu9244@yahoo.com>
2022-12-15 09:15:15 -0800

    digit 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints 0 to 9
+ * Return: multiplication result of the 2 inputs
+ */
+void print_numbers(void);
+{
+int x;
+
+	for (x = 0; x <= 9; x++)
+		_putchar(x + '0');
+			_putchar('\n');
+}
