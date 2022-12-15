54b3aee9d2c9549a912b7d92f225eb108d1c6ca6
EsheBT <eshetu9244@yahoo.com>
2022-12-15 05:57:09 -0800

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,12 @@
 /**
  * mul - is a function for two number multiplication
  * @a:input
- * @b:input
+ * @b:anathor input
  * Return:always return result
  */
 int mul(int a, int b);
 {
-m = 0;
-m = a * b;
-printf("%d", m)
-return (0);
+
+return (a * b);
+
 }
