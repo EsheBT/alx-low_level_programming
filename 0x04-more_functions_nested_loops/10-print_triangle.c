cb1ea1dd6bd6056669f03e23a44f4d2d73c339cd
EsheBT <eshetu9244@yahoo.com>
2022-12-16 10:43:23 -0800

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 		for (a = size; a > 0; a--)
 		{
 			for (b = 1; b <= size; b++)
-				if (b >= i)
+				if (b >= b)
 					_putchar('#');
 				else
 					_putchar(' ');
