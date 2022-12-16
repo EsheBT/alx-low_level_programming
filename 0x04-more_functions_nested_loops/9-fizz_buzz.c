452fb10018c09fdf1b92503254f9fd2c63d71797
EsheBT <eshetu9244@yahoo.com>
2022-12-16 09:50:49 -0800

    number fizz buzz fizzbuzz mixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,8 +16,9 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if (x == 100)
+			printf("%s", b)
+		else if ((x % 3 == 0) && (x % 5 == 0))
 			printf("%s ", ab);
 		else if (i % 3 == 0)
 			printf("%s ", a);
