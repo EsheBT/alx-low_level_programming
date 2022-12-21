1e0fe58c8be25a4a84b82de6b95ebc386e2bbab6
EsheBT <eshetu9244@yahoo.com>
2022-12-21 11:07:53 -0800

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @s1: first string input
  * @s2:second string
  *
- * Return:always 0
+ * Return: always 0
  */
 
 char *_strcat(char *s1, char *s2)
@@ -15,8 +15,8 @@ int i, j;
 for (i = 0; s1[i] != '\0'; i++)
 for (j = 0; s2[j] != '\0'; j++)
 {
-	s1[i] = s2[j]
-		i++
+	s1[i] = s2[j];
+		i++;
 }
 s1[i] = '\0';
 return (s1);
