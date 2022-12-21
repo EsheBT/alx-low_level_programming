cd74df42dfabb1c57c88ab75d2542155a36b0e5e
EsheBT <eshetu9244@yahoo.com>
2022-12-21 10:43:05 -0800

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat:function that concatenate two strings
+ * _strcat-function that concatenate two strings
  *
  * @s1: first string input
  * @s2:second string
@@ -10,12 +10,14 @@
  */
 
 char *_strcat(char *s1, char *s2)
-
-for (int i = 0; s1[i] != '\0'; i++)
-for (int j = 0; s2[j] != '\0'; j++)
+{
+int i, j;
+for (i = 0; s1[i] != '\0'; i++)
+for (j = 0; s2[j] != '\0'; j++)
 {
 	s1[i] = s2[j]
 		i++
 }
 s1[i] = '\0';
-return (0);
+return (s1);
+}
