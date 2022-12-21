1b6deaf69a94ac161d53815026c1afe32868c63f
EsheBT <eshetu9244@yahoo.com>
2022-12-21 11:19:08 -0800

    concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	int i = 0, j = 0;
 
-	i = 0;
+	while (dest[i++]
+			j++;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+			for (i = 0; src[i] && i < n; i++)
+			dest[j++] = src[i];
+
+			return (dest);
+			}
