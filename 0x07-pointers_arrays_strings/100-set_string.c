fdf722e43bb70ec9ed9e416821c08455ac868dba
EsheBT <eshetu9244@yahoo.com>
2023-01-02 10:58:39 -0800

    set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,11 @@
-#include"main.h"
-#include<stdio.h>
-
+#include "main.h"
+#include <stdio.h>
 /**
  * set_string - redirect.
  * @s: double pointer to address
  * @to: the original pointer
  *
- * Return: assigned.
+ * Return: chessboard.
  */
 void set_string(char **s, char *to)
 {
