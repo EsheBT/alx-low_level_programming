1c25e7b7d95c0bdcff0e312ca0a6d3fca2cccbda
EsheBT <eshetu9244@yahoo.com>
2023-01-12 09:47:58 -0800

    concatenate s1&s2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the maximum number of bytes to use from s2
+ *
+ * Return: If memory allocation fails, return NULL. Otherwise, return a pointer
+ * to the first character of the new string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *cat;
+	unsigned int l1 = 0;
+	unsigned int l2 = 0;
+
+	if (s1)
+	{
+		while (s1[l1])
+			++l1;
+	}
+
+	if (s2)
+	{
+		while (l2 < n && s2[l2])
+			++l2;
+	}
+
+	cat = malloc(sizeof(char) * l1 + l2 + 1);
+	if (!cat)
+		return (NULL);
+
+	for (n = 0; n < l1; ++n, ++cat)
+		*cat = s1[n];
+
+	for (n = 0; n < l2; ++n, ++cat)
+		*cat = s2[n];
+
+	*cat = '\0';
+
+	return (cat);
+}
